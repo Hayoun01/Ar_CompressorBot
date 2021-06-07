2581b2d4bbdf9b38c793917b0ff5480c6759ce55
Hayoun01 <37371074+Hayoun01@users.noreply.github.com>
2021-06-07 20:52:02 +0100

    Update funcn.py

--- helper/funcn.py
+++ helper/funcn.py
@@ -79,13 +79,13 @@ async def progress(current, total, event, start, type_of_ps, file=None):
         speed = current / diff
         time_to_completion = round((total - current) / speed) * 1000
         progress_str = "`[{0}{1}] {2}%`\n\n".format(
-            "".join(["█" for i in range(math.floor(percentage / 5))]),
-            "".join(["░" for i in range(20 - math.floor(percentage / 5))]),
+            "".join(["●" for i in range(math.floor(percentage / 5))]),
+            "".join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2),
         )
         tmp = (
             progress_str
-            + "`{0} of {1}`\n\n`✦ السرعة: {2}/s`\n\n`✦ الوقت المتبقي: {3}`\n\n".format(
+            + "`{0} of {1}`\n\n`✦ Speed: {2}/s`\n\n`✦ ETA: {3}`\n\n".format(
                 hbs(current),
                 hbs(total),
                 hbs(speed),
